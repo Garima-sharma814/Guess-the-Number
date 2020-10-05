f85a9a21083bc29e9352d11e540144a711c0231c
Garima <sharmagarima814@gmail.com>
2020-10-05 21:53:42 +0530

    initial commit

---
+++ guessthenumber.py
@@ -0,0 +1,18 @@
+# guess the number
+guess = 5
+n = 68
+while(guess <= 5):
+    print("number of guesses left:- ", guess)
+    i = int(input("enter the number "))
+    if i > n:
+        print("WRONG! \n your no. is greater than the actual number\n")
+        guess -= 1
+    if i < n:
+        print("WRONG! \n your no. is less than the actual number\n")
+        guess -= 1
+    if i == n:
+        print("Yea !! \n quiet smart huh \n you won ")
+        break
+    if guess == 0:
+        print("game over \n you lose")
+        break
